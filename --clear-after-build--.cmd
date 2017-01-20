5d5522820626afd6599ecd7d2c56d3573032fd5d
zamgi <denisov72@mail.ru>
2017-01-20 14:11:29 +0300

    Update --clear-after-build--.cmd

--- --clear-after-build--.cmd
+++ --clear-after-build--.cmd
@@ -1,5 +1,5 @@
-del "*.suo" /Q 2> nul
-del "ss.web.demo\*.pdb" /Q 2> nul
-rd "ss.web.demo\obj" /S/Q 2> nul
-rd "SentSplitter\bin" /S/Q 2> nul
-rd "SentSplitter\obj" /S/Q 2> nul
+del "*.suo" /Q
+del "ss.web.demo\*.pdb" /Q
+rd "ss.web.demo\obj" /S/Q
+rd "SentSplitter\bin" /S/Q
+rd "SentSplitter\obj" /S/Q
