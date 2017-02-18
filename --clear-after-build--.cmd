3e66e8ce8d6aa2967e979c51ee4f64cb6edd1c0c
zamgi <denisov72@mail.ru>
2017-02-18 03:06:00 +0300

    Update --clear-after-build--.cmd

--- --clear-after-build--.cmd
+++ --clear-after-build--.cmd
@@ -1,6 +1,6 @@
-del "*.suo" /Q/A
+del "*.suo" /S/Q/F/A
+del "*.csproj.user" /S/Q/F/A
 del "ss.web.demo\bin\*.pdb" /Q
-del "ss.web.demo\*.csproj.user" /Q
 rd "ss.web.demo\obj" /S/Q
 rd "SentSplitter\bin" /S/Q
 rd "SentSplitter\obj" /S/Q
